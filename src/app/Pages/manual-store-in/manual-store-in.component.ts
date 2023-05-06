4459b7642d9e51bd5748c17a766d308cbff5936a
DELL <DELL@DESKTOP-6TQE5G7>
2023-05-06 14:12:59 +0530

    Alpesh_Changes_06_05_2023

--- src/app/Pages/manual-store-in/manual-store-in.component.ts
+++ src/app/Pages/manual-store-in/manual-store-in.component.ts
@@ -16,6 +16,7 @@ import { MyService } from 'src/app/services/api.service';
 import { MaterialCodeDescriptionComponent } from '../material-code-description/material-code-description.component';
 import { VendorMatCodeDescriptionComponent } from '../vendor-mat-code-description/vendor-mat-code-description.component';
 import { MatTableDataSource } from '@angular/material/table';
+import { DatePipe } from '@angular/common';
 
 
 @Component({
@@ -32,7 +33,7 @@ export class ManualStoreInComponent {
   });
 
   radioItems: any = [];
-  chkBox: any= [];
+  chkBox: any = [];
   m_StatusItems: any = [];
 
   materialCategory: string = "";
@@ -58,28 +59,32 @@ export class ManualStoreInComponent {
   uomKG: string = "Kg";
   uomMeter: string = "Meter";
   uomNos: string = "Nos";
-  materialTypeList: any=[];
-  isFieldreadonly=true;
-  fieldreadonly=true;
+  materialTypeList: any = [];
+  isFieldreadonly = true;
+  fieldreadonly = true;
 
-  lengthreadonly=true;
-  weightReadonly=true;
-  noOfSoolReadonly=true;
-  
+  doedate = new Date();
+  domdate = new Date();
+
+  lengthreadonly = true;
+  weightReadonly = true;
+  noOfSoolReadonly = true;
+
 
   selectedRowDataMaterialCOD: any;
   selectedRowDataMaterialDESC: any;
-  selectedRowDataVendorCOD:any;
-  selectedRowDataVendorDESC:any;
-  materialData_list:any;
+  selectedRowDataVendorCOD: any;
+  selectedRowDataVendorDESC: any;
+  materialData_list: any;
 
   @ViewChild(MaterialCodeDescriptionComponent) materialComponent!: MaterialCodeDescriptionComponent;
   @ViewChild(VendorMatCodeDescriptionComponent) vendorComponent!: VendorMatCodeDescriptionComponent;
   @ViewChild('material') material!: MaterialCodeDescriptionComponent;
   @ViewChild('vendor') vendor!: MaterialCodeDescriptionComponent;
-  dataSource!:MatTableDataSource<any>;
+  dataSource!: MatTableDataSource<any>;
+  shelfLife: any;
 
-  constructor(public fb: UntypedFormBuilder, private router: Router, private service: MyService) {
+  constructor(public fb: UntypedFormBuilder, private router: Router, private service: MyService,private datePipe: DatePipe) {
 
     this.chkBox = ['Yes', 'No'];
   }
@@ -108,9 +113,9 @@ export class ManualStoreInComponent {
       projectID: ['', [Validators.required]],
       dor: ['', [Validators.required]],
       grnNo: ['', [Validators.required]],
-       uomKG: ['', [Validators.required]],
-       uomMeter: ['', [Validators.required]],
-       uomNos: ['', [Validators.required]],
+      uomKG: ['', [Validators.required]],
+      uomMeter: ['', [Validators.required]],
+      uomNos: ['', [Validators.required]],
 
     });
 
@@ -127,7 +132,7 @@ export class ManualStoreInComponent {
       debugger
       this.m_StatusItems = res;
     });
- 
+
   }
 
   get formControl() {
@@ -172,87 +177,90 @@ export class ManualStoreInComponent {
   }
 
 
- notFoundMessage:any;
- applyfilterformaterial(event:Event){
-   debugger
-   const filtervalue=(event.target as HTMLInputElement).value
-   this.material.dataSource.filter=filtervalue.trim().toLowerCase();
-   if(this.material.dataSource.paginator)
-   {
-     this.material.dataSource.paginator.firstPage();
-   }
-   if(this.material.dataSource.filteredData.length==0)
-   {
-   this.notFoundMessage = 'No matching data found';
-   }
-   else {
-     this.notFoundMessage = '';
-   }
- }
-
- applyfilterforvendor(event:Event){
-  debugger
-  const filtervalue=(event.target as HTMLInputElement).value
-  this.vendor.dataSource.filter=filtervalue.trim().toLowerCase();
-  if(this.vendor.dataSource.paginator)
-  {
-    this.vendor.dataSource.paginator.firstPage();
-  }
-  if(this.vendor.dataSource.filteredData.length==0)
-  {
-  this.notFoundMessage = 'No matching data found';
-  }
-  else {
-    this.notFoundMessage = '';
-  }
-}
-
- onCategoryClick(val:any){
-  debugger;
-  this.materialCategory = val.GRP_COD;
-  this.service.GetMaterialData().subscribe(resp =>{
-    this.materialData_list = resp;
-    this.materialData_list=this.materialData_list.filter((x: any) => x.PRD_GRP_COD === val.GRP_COD)
-    this.isFieldreadonly=false
-    //console.log(this.materialData_list);
-    // this.material.dataSource=this.materialData_list
-    // this.materialComponent.callmateial();
-  })
-if(val.GRP_COD=='SW'){
-  this.lengthreadonly=false;
-  this.noOfSoolReadonly=false;
-  this.weightReadonly=true;
-
-}
-if(val.GRP_COD=='DF'){
-  this.weightReadonly=false;
-  this.lengthreadonly=false;
-  this.noOfSoolReadonly=true;
-}
-
- }
-
-  onContainerClick(){
-  debugger
-  this.material.closeTable();
-  this.vendor.closeTable();
-  }
-  
-  OnclickMaterial(){
+  notFoundMessage: any;
+  applyfilterformaterial(event: Event) {
     debugger
-    const data =this.materialComponent.onclick();
+    const filtervalue = (event.target as HTMLInputElement).value
+    this.material.dataSource.filter = filtervalue.trim().toLowerCase();
+    if (this.material.dataSource.paginator) {
+      this.material.dataSource.paginator.firstPage();
+    }
+    if (this.material.dataSource.filteredData.length == 0) {
+      this.notFoundMessage = 'No matching data found';
+    }
+    else {
+      this.notFoundMessage = '';
+    }
+  }
+
+  applyfilterforvendor(event: Event) {
+    debugger
+    const filtervalue = (event.target as HTMLInputElement).value
+    this.vendor.dataSource.filter = filtervalue.trim().toLowerCase();
+    if (this.vendor.dataSource.paginator) {
+      this.vendor.dataSource.paginator.firstPage();
+    }
+    if (this.vendor.dataSource.filteredData.length == 0) {
+      this.notFoundMessage = 'No matching data found';
+    }
+    else {
+      this.notFoundMessage = '';
+    }
+  }
+
+  onCategoryClick(val: any) {
+    debugger;
+    this.shelfLife = val.PRD_LENG;
+    this.materialCategory = val.GRP_COD;
+    this.service.GetMaterialData().subscribe(resp => {
+      this.materialData_list = resp;
+      this.materialData_list = this.materialData_list.filter((x: any) => x.PRD_GRP_COD === val.GRP_COD)
+      this.isFieldreadonly = false
+      //console.log(this.materialData_list);
+      // this.material.dataSource=this.materialData_list
+      // this.materialComponent.callmateial();
+    })
+    if (val.GRP_COD == 'SW') {
+      this.lengthreadonly = false;
+      this.noOfSoolReadonly = false;
+      this.weightReadonly = true;
+
+    }
+    if (val.GRP_COD == 'DF') {
+      this.weightReadonly = false;
+      this.lengthreadonly = false;
+      this.noOfSoolReadonly = true;
+    }
+  }
+
+  onDropdownClick(val: any) {
+    debugger
+    if (val == 'REGULAR') {
+      this.materialStatus = 'HOLD';
+    }
+  }
+
+  onContainerClick() {
+    this.material.closeTable();
+    this.vendor.closeTable();
+  }
+
+  OnclickMaterial() {
+    debugger
+    const data = this.materialComponent.onclick();
     console.log(data);
   }
 
-  OnclickVendor(){
+  OnclickVendor() {
     debugger
-    const data =this.vendorComponent.onclick();
+    const data = this.vendorComponent.onclick();
     console.log(data);
   }
-   
+
+
   onSelectedRowofMaterial(selectedRowData: any) {
     debugger
-    console.log (selectedRowData);
+    console.log(selectedRowData);
     this.selectedRowDataMaterialCOD = selectedRowData.PRD_COD;
     this.selectedRowDataMaterialDESC = selectedRowData.PRD_DESC;
     // do something with the selected row data
@@ -260,7 +268,7 @@ if(val.GRP_COD=='DF'){
 
   onSelectedRowofVendor(selectedRowVendor: any) {
     debugger
-    console.log (selectedRowVendor);
+    console.log(selectedRowVendor);
     this.selectedRowDataVendorCOD = selectedRowVendor.VED_COD;
     this.selectedRowDataVendorDESC = selectedRowVendor.VED_DESC;
     // do something with the selected row data
@@ -269,7 +277,31 @@ if(val.GRP_COD=='DF'){
   Reset() {
     this.storeInForm.reset();
   }
-  
- 
+
+  onDateChage(val: any) {
+    debugger
+    this.shelfLife;
+    this.doedate.setDate(val + 5);
+
+    console.log(this.doedate)
+
+    const date = new Date(val); // Current date
+    const date1 = new Date(); // Current date
+
+    const numberOfDaysToAdd = 5;
+    date1.setDate(date.getDate() + numberOfDaysToAdd);
+
+
+
+    const datePipe = new DatePipe('en-US');
+    const formattedDate = this.datePipe.transform(date1, 'dd-MM-yyyy');
+
+    // this.doedate = formattedDate;
+
+    console.log(date1);
+    console.log(formattedDate);
+
+  }
+
 }
 
