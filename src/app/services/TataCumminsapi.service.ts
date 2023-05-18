6dc25ecb25e75501e345e64eec46c74c04d8cccb
itnextsolutions <itnextsolutions.web@gmail.com>
2023-05-18 18:26:58 +0530

    Yogesh Chnges

--- src/app/services/TataCumminsapi.service.ts
+++ src/app/services/TataCumminsapi.service.ts
@@ -9,8 +9,17 @@ export class TataService {
   apiEndpoint = 'https://localhost:44363/api/';
 
 public getemptypalletout(val:any){
-  debugger
   const params = { parameter: val}
   return this.http.get(this.apiEndpoint+'EmptyPalletOut/GetEmptyPalletOut',{params})
+}
+
+GetStockCountPartNo(){
+  return this.http.get(this.apiEndpoint+'StockCount/GetStockCount')
+}
+
+GetPalletDetails(val1:any,val2:any){
+  debugger
+  const params = { partno: val1,grno:val2}
+  return this.http.get(this.apiEndpoint+'StockCount/GetPalletDetails',{params})
 }
 }
