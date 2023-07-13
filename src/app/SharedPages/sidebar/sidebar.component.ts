33c361c489148d702f5cedf34f57418c545f36cf
itnextsolutions <itnextsolutions.web@gmail.com>
2023-07-13 11:54:09 +0530

    Yogesh Changes

--- src/app/SharedPages/sidebar/sidebar.component.ts
+++ src/app/SharedPages/sidebar/sidebar.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, Renderer2 } from '@angular/core';
+import { MyService } from 'src/app/services/api.service';
 
 @Component({
   selector: 'app-sidebar',
@@ -6,5 +7,44 @@ import { Component } from '@angular/core';
   styleUrls: ['./sidebar.component.css']
 })
 export class SidebarComponent {
+
+  MenuList:any=[];
+  child :any=[];
+  //@ViewChild('tatacumminssidebar') sidebarRef!: ElementRef;
+  isSidebarOpen = false;
+
+constructor(private router:MyService,private renderer: Renderer2){}
+
+  ngOnInit(){
+    var userid=localStorage.getItem("User_Id")
+    return this.router.GetMenuList(userid).subscribe(resp=>{
+      this.MenuList=resp
+      const listString = JSON.stringify(this.MenuList);
+      localStorage.setItem("menu",listString)
+      
+  this.closeSidebar()
+  })
+  }
+ 
+
+  openSidebar(): void {
+    
+    this.isSidebarOpen = true;
+  }
+
+  toggleSidebar(){
+    this.isSidebarOpen=!this.isSidebarOpen;
+  }
+
+  closeSidebar() {
+    
+    this.isSidebarOpen = false;
+  }
+
+  trigger(val:any){
+   
+    this.child=this.MenuList.filter((x: any) => x.OP_PAR6 == val)
+  }
+
 
 }
